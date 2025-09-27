825a39776c70be8ba7c7849ae55f7f962c7fb6b0
Manuel1317-jpg <josemanuelzempoaltecatl26@gmail.com>
2025-09-27 16:01:21 -0600

    "quite cuenta total"

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -3,7 +3,7 @@ public class Cliente{
 
     private String nombre;
     private String correo;
-    private double cuentaTotal;
+    
 
     public Cliente(String nombre, String correo) {
         this.nombre = nombre;
@@ -26,41 +26,11 @@ public class Cliente{
         this.correo = correo;
     }
 
-    public void comprarComida(double costoComida) {
-        if (costoComida > 0) {
-            this.cuentaTotal += costoComida;
-            System.out.println(this.nombre + " ha comprado comida. La nueva cuenta es: $" + this.cuentaTotal);
-        } else {
-            System.out.println("Error: El costo de la comida debe ser positivo.");
-        }
-    }
-
-    public void comprarBebida(double costoBebida) {
-        if (costoBebida > 0) {
-            this.cuentaTotal += costoBebida;
-            System.out.println(this.nombre + " ha comprado una bebida. La nueva cuenta es: $" + this.cuentaTotal);
-        } else {
-            System.out.println("Error: El costo de la bebida debe ser positivo.");
-        }
-    }
-
-    public void rentarOComprarLibro(double costoLibro, boolean esRenta) {
-        if (costoLibro > 0) {
-            String tipoTransaccion = esRenta ? "rentado" : "comprado";
-            this.cuentaTotal += costoLibro;
-            System.out.println(this.nombre + " ha " + tipoTransaccion + " un libro. La nueva cuenta es: $" + this.cuentaTotal);
-        } else {
-            System.out.println("Error: El costo del libro debe ser positivo.");
-        }
-    }
-
-
     // Opcional pero recomendado: Método toString() para una mejor representación
     public String toString() {
         return "Cliente{" +
                "nombre='" + nombre + '\'' +
                ", correo='" + correo + '\'' +
-               ", cuentaTotal=" + cuentaTotal +
                '}';
     }
 
