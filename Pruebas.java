8c8cb9fe771a362d7af4962739b0e09a9f15fa13
Manuel1317-jpg <josemanuelzempoaltecatl26@gmail.com>
2025-09-27 20:19:57 -0600

    "MENU PRINCIPAL"

--- Pruebas.java
+++ Pruebas.java
@@ -1,9 +1,35 @@
 import Articulos.Inventario;
-import Menus.MenuAdmin;
+import Menus.*;
+import java.util.Scanner;
 public class Pruebas{
 	public static void main(String[] args) {
 		Inventario inventario = new Inventario();
+		Scanner scanner= new Scanner(System.in);
 		MenuAdmin menuAdmin=new MenuAdmin(inventario);
-		menuAdmin.mostrarMenu();
+		MenuUsuario menuUser=new MenuUsuario(inventario);
+		int opcion=0;
+		do{
+			System.out.println("--BIENVENIDO Al SISTEMA DE LA LIBRERIA-CAFE DE LA FI--");
+			System.out.println("1.Menu de administracion");
+			System.out.println("2.Menu de Usuario");
+			System.out.println("3.Salir");
+			System.out.println("Ingrese una opcion");
+			opcion=scanner.nextInt();
+			scanner.nextLine();
+			switch (opcion) {
+				case 1:
+					menuAdmin.mostrarMenu();
+					break;
+				case 2:
+					menuUser.mostrarMenuUsuario();
+					break;
+				case 3:
+					System.out.println("Salio del progrma");
+					break;
+				default:
+					System.out.println("Escoja una opcion valida");
+					break;
+			}
+		}while(opcion!=3);
 	}
 }
