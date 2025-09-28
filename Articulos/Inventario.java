08f5b9c63bd650019590c35ed77e26fe781ec4c9
gabrielss-web <gsalaiss14@gmail.com>
2025-09-28 17:30:48 -0600

    "acentos"

--- Articulos/Inventario.java
+++ Articulos/Inventario.java
@@ -11,14 +11,14 @@ public class Inventario{
 	public Inventario()
     {
         //libros
-        Libro l1 = new Libro("Cien años de soledad", 496, "Sudamericana", "Gabriel García Márquez", 250.50, 10);
-        Libro l2 = new Libro("El principito", 96, "Salamandra", "Antoine de Saint-Exupéry", 150.00, 25);
+        Libro l1 = new Libro("Cien años de soledad", 496, "Sudamericana", "Gabriel Garcia Márquez", 250.50, 10);
+        Libro l2 = new Libro("El principito", 96, "Salamandra", "Antoine de Saint-Exupery", 150.00, 25);
         Libro l3 = new Libro("1984", 328, "Debolsillo", "George Orwell", 180.75, 15);
-        Libro l4 = new Libro("Don Quijote de la Mancha", 1072, "Edebé", "Miguel de Cervantes", 300.00, 5);
+        Libro l4 = new Libro("Don Quijote de la Mancha", 1072, "Edebe", "Miguel de Cervantes", 300.00, 5);
         Libro l5 = new Libro("El señor de los anillos", 1216, "Minotauro", "J.R.R. Tolkien", 450.20, 8);
-        Libro l6 = new Libro("Orgullo y prejuicio", 432, "Penguin Clásicos", "Jane Austen", 125.00, 20);
-        Libro l7 = new Libro("Crónica de una muerte anunciada", 128, "Diana", "Gabriel García Márquez", 140.00, 12);
-        Libro l8 = new Libro("La sombra del viento", 574, "Planeta", "Carlos Ruiz Zafón", 220.50, 18);
+        Libro l6 = new Libro("Orgullo y prejuicio", 432, "Penguin Clasicos", "Jane Austen", 125.00, 20);
+        Libro l7 = new Libro("Cronica de una muerte anunciada", 128, "Diana", "Gabriel Garcia Márquez", 140.00, 12);
+        Libro l8 = new Libro("La sombra del viento", 574, "Planeta", "Carlos Ruiz Zafon", 220.50, 18);
         Libro l9 = new Libro("Fahrenheit 451", 208, "Minotauro", "Ray Bradbury", 160.00, 10);
         Libro l10 = new Libro("Harry Potter", 256, "Salamandra", "J.K. Rowling", 280.00, 30);
 
@@ -33,8 +33,8 @@ public class Inventario{
         this.libros.put(l9.getId(), l9);
         this.libros.put(l10.getId(), l10);
 
-        Comida c1 = new Comida("Sandwich de jamón", "grande", 55, "Sandwich", 20);
-        Comida c2 = new Comida("Ensalada César", "mediano", 70, "Ensalada", 15);
+        Comida c1 = new Comida("Sandwich de jamon", "grande", 55, "Sandwich", 20);
+        Comida c2 = new Comida("Ensalada Cesar", "mediano", 70, "Ensalada", 15);
         Comida c3 = new Comida("Pastel de chocolate", "chico", 45, "Postre", 30);
         Comida c4 = new Comida("Galletas de avena", "chico", 20, "Postre", 50);
         Comida c5 = new Comida("Sopa de lentejas", "mediano", 60, "Sopa", 10);
@@ -55,16 +55,16 @@ public class Inventario{
         this.comidas.put(c9.getId(), c9);
         this.comidas.put(c10.getId(), c10);
 
-        Bebida b1 = new Bebida("Café Americano", "Expresso", "mediano", 35, "Caliente", 50);
-        Bebida b2 = new Bebida("Té Helado", "Limón", "grande", 40, "Frío", 40);
-        Bebida b3 = new Bebida("Jugo de Naranja", "Naranja", "chico", 45, "Frío", 30);
+        Bebida b1 = new Bebida("Cafe Americano", "Expresso", "mediano", 35, "Caliente", 50);
+        Bebida b2 = new Bebida("Te Helado", "Limon", "grande", 40, "Frio", 40);
+        Bebida b3 = new Bebida("Jugo de Naranja", "Naranja", "chico", 45, "Frio", 30);
         Bebida b4 = new Bebida("Capuchino", "Leche", "mediano", 40, "Caliente", 60);
-        Bebida b5 = new Bebida("Refresco", "Cola", "grande", 30, "Frío", 70);
+        Bebida b5 = new Bebida("Refresco", "Cola", "grande", 30, "Frio", 70);
         Bebida b6 = new Bebida("Chocolate Caliente", "Chocolate", "mediano", 45, "Caliente", 35);
-        Bebida b7 = new Bebida("Té Verde", "Verde", "chico", 30, "Caliente", 45);
-        Bebida b8 = new Bebida("Limonada", "Limón", "grande", 35, "Frío", 55);
+        Bebida b7 = new Bebida("Te Verde", "Verde", "chico", 30, "Caliente", 45);
+        Bebida b8 = new Bebida("Limonada", "Limon", "grande", 35, "Frio", 55);
         Bebida b9 = new Bebida("Mokachino", "Moka", "mediano", 35, "Caliente", 20);
-        Bebida b10 = new Bebida("Jugo de Manzana", "Manzana", "chico", 45, "Frío", 25);
+        Bebida b10 = new Bebida("Jugo de Manzana", "Manzana", "chico", 45, "Frio", 25);
 
         this.bebidas.put(b1.getId(), b1);
         this.bebidas.put(b2.getId(), b2);
@@ -107,7 +107,7 @@ public class Inventario{
 
     public void mostrarLibros()
     {
-        System.out.printf("%-35s %-10s %-10s\n", "TÍTULO DEL LIBRO", "ID","PRECIO");
+        System.out.printf("%-35s %-10s %-10s\n", "TITULO DEL LIBRO", "ID","PRECIO");
         for (int i=1; i<=libros.size(); i++)
             System.out.printf("%-35s %-10d %-10f\n",libros.get(i).getTituloLibro(),libros.get(i).getId(), libros.get(i).getPrecio());
     }
